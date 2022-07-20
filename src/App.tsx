68ed60c9f0c1ed8f40141ce5da6274722b516998
Excession27 <feboobef27@gmail.com>
2022-07-20 16:57:33 +0200

    Initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,31 @@
-import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Swiper from './components/Swiper';
 
 function App() {
+  const items = [
+    {
+      imageSrc: "https://framed.shindiristudio.com/wp-content/uploads/2016/04/Accordion-2.jpg",
+      imageAlt: "A person's eye",
+    },
+    {
+      imageSrc: "https://framed.shindiristudio.com/wp-content/uploads/2016/04/Accordion-5.jpg",
+      imageAlt: 'A rock formation',
+    },
+    {
+      imageSrc: "https://framed.shindiristudio.com/wp-content/uploads/2016/04/Accordion-3.jpg",
+      imageAlt: 'Some flowers',
+    },
+    {
+      imageSrc: "https://framed.shindiristudio.com/wp-content/uploads/2016/04/Accordion-4.jpg",
+      imageAlt: 'An egyptian wall painting',
+    }
+  ];
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      
+      <Swiper items={items} sliderWidth={600} layout={"center"}/>
+
     </div>
   );
 }
